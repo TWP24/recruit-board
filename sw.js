c3e9543cc4efa7cd8c5f2f0bb0f25c4a2fdcd35b
recruit-board publisher <noreply@users.noreply.github.com>
2026-09-16 08:27:27 +0100

    board: 2026-09-16 07:27Z

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-/* Recruiting board service worker - build 202609150610 */
-const CACHE = 'recruit-202609150610';
+/* Recruiting board service worker - build 202609160727 */
+const CACHE = 'recruit-202609160727';
 const ASSETS = ['./', './index.html', './manifest.webmanifest',
                 './icon-192.png', './icon-512.png', './apple-touch-icon.png'];
 
